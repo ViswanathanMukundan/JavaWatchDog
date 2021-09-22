f3cf851b99b1a11003c016200420081f4d9bd239
ViswanathanMukundan <50147500+ViswanathanMukundan@users.noreply.github.com>
2021-09-22 08:43:50 +0530

    Update MainClass.java

--- src/com/mv/fileWatchDog/MainClass.java
+++ src/com/mv/fileWatchDog/MainClass.java
@@ -1,25 +1,27 @@
 package com.mv.fileWatchDog;
 
+/*
+MAIN CLASS TO MAKE USE OF THE OTHER UTILITY CLASSES.
+*/
+
 import java.io.FileNotFoundException;
 import java.util.Date;
-
 import javax.mail.MessagingException;
 
 public class MainClass {
 	
 	public static void main(String[] args) throws FileNotFoundException, MessagingException
 	{
-		String filePath = "PATH OF THE FILE TO BE MONITORED";
-		FileWatcher watchdog = new FileWatcher(filePath);
+		String filePath = "PATH OF THE FILE TO BE MONITORED"; //INSERT THE FULLY QUALIFIED PATH OF THE FILE INTO THIS VARIABLE
+		FileWatcher watchdog = new FileWatcher(filePath);  //CALLING THE FILEWATCHER CLASS WITH THE FILEPATH AS CONSTRUCTOR ARGUMENT
 		Mailer mailMan = new Mailer();
-		while(true)
+		while(true) //PUTTING THE IMPLEMENTATION UNDER AN INFINITE WHILE-LOOP FOR NOW. FURTHER IMPROVEMENTS MIGHT INCLUDE A MORE EFFICIENT IMPLEMENTATION, (MAYBE AS A BACKGROUND THREAD)
 		{
 			if(watchdog.isFileUpdated(filePath))
 			{
 				System.err.println("File updated");
 				watchdog.writer.addEntry("File updated at::: " + new java.text.SimpleDateFormat("dd/MM/yyyy, HH:mm:ss").format(new Date(watchdog.getTimeStamp())));
-				//mailMan.sendEmail("Reg. log readings.", "Unusual activity has been recorded in the log. Time of recording::: " + new java.text.SimpleDateFormat("dd/MM/yyyy, HH:mm:ss").format(new Date(watchdog.getTimeStamp())) + ". Kindly note and take action. Regards, admin.");
-				mailMan.sendEmail("MAIL SUBJECT", "MAIL BODY");
+				mailMan.sendEmail("MAIL SUBJECT", "MAIL BODY"); //PASS EMAIL SUBJECT AND EMAIL BODY AS PARAMETERS. USE '\n' TO DENOTE LINE BREAKS.
 			}
 		}
 	}
