6e0e55c60ff4a01401446b82cde390cdb7eb3824
ViswanathanMukundan <50147500+ViswanathanMukundan@users.noreply.github.com>
2021-09-22 08:46:10 +0530

    Update logEntry.java

--- src/com/mv/fileWatchDog/logEntry.java
+++ src/com/mv/fileWatchDog/logEntry.java
@@ -6,14 +6,15 @@ import java.util.Date;
 import java.nio.file.*;
 
 /*
- * CLASS TO ADD AN ENTRY INTO THE LOG FILE WHOSE SOURCE IS SPECIFIED 
+ *THIS CLASS IS USED TO ADD ENTRIES INTO YET ANOTHER LOG FILE. AN ASSUMED USE-CASE CAN BE TAKEN AS- RECORDING ONLY THE CRITICAL WARNINGS AND ERRORS IN ORDER TO MEASURE THE 
+ EFFICIENCY OF THE SYSTEM.
  * */
 
 
 
 public class logEntry {
 	
-	private static String LOG_SOURCE_FILE = "PATH TO THE LOG FILE";
+	private static String LOG_SOURCE_FILE = "PATH TO THE LOG FILE"; //THE FULLY-QUALIFIED PATH TO THE LOG FILE (OR MAYBE CONFIG FILE) IS SPECIFIED IN THIS VARIABLE.
 	Path fileName = Path.of(LOG_SOURCE_FILE);
 	SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy::: HH:mm:ss");
 	
@@ -22,7 +23,7 @@ public class logEntry {
 		byte bytes[] = ("\r\n" + formatter.format(new Date()) + ": " + entry.toString()).getBytes();
 		try 
 		{
-			Files.write(fileName, bytes, StandardOpenOption.APPEND); //APPEND THE ENTRY INSTEAD OF OVERWRITING THE EXISTING ONES.
+			Files.write(fileName, bytes, StandardOpenOption.APPEND);  //APPEND THE ENTRY INSTEAD OF OVERWRITING THE EXISTING ONES.
 		}
 		catch (IOException e) 
 		{
